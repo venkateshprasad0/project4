3871126a0aec2885e2d17f17d8c137d72fd82681
Venkatesh Prasad V <venkateshprasad0@gmail.com>
2022-06-30 15:18:31 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project4';
+  messages: any[] = [];
+  subscription: Subscription;
+
+  constructor(private messageService: MessageService) {
+    // subscribe to home component messages
+    this.subscription = this.messageService.onMessage().subscribe(message => {
+      if (message) {
+        this.messages.push(message);
+      } else {
+        // clear messages when empty message received
+        this.messages = [];
+      }
+    });
+  }
+  ngOnDestroy() {
+    // unsubscribe to ensure no memory leaks
+    this.subscription.unsubscribe();
+  }
 }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private messageService: MessageService) { }
+
+  sendMessage():void{
+    this.messageService.sendMessage("Message from home component to app component");
+  }
+
+  clearMessages(): void{
+    this.messageService.clearMessages();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
